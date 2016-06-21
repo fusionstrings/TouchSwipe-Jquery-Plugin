f709b31917aa752bfdd9c236e91f50dc754242f3
Dilip Kr. Shukla <contact@fusionstrings.com>
2016-06-21 19:14:07 +0200

    Update jquery.touchSwipe.js

--- jquery.touchSwipe.js
+++ jquery.touchSwipe.js
@@ -147,19 +147,33 @@
  * @memberOf $
  */
 
-
-(function(factory) {
-  if (typeof define === 'function' && define.amd && define.amd.jQuery) {
-    // AMD. Register as anonymous module.
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
     define(['jquery'], factory);
-  } else if (typeof module !== 'undefined' && module.exports) {
-    // CommonJS Module
-    factory(require("jquery"));
+  } else if (typeof module === 'object' && module.exports) {
+    // Node/CommonJS
+    module.exports = function( root, jQuery ) {
+      if ( jQuery === undefined ) {
+        // require('jQuery') returns a factory that requires window to
+        // build a jQuery instance, we normalize how we use modules
+        // that require this pattern but the window provided is a noop
+        // if it's defined (how jquery works)
+        if ( typeof window !== 'undefined' ) {
+          jQuery = require('jquery');
+        }
+        else {
+          jQuery = require('jquery')(root);
+        }
+      }
+      factory(jQuery);
+      return jQuery;
+    };
   } else {
-    // Browser globals.
+    // Browser globals
     factory(jQuery);
   }
-}(function($) {
+}(function ($) {
   "use strict";
 
   //Constants
